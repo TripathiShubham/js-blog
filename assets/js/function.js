7183f2a5165d0788b7ebd23610dc2ba4307a7a2b
Shubham Tripathi <shubhamio.js@gmail.com>
2019-03-31 14:12:45 +0530

    Adding scroll functionality

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,63 +1,96 @@
-(function() {
-    var isOpen = false;
+(function () {
+  let isOpen = false;
+  let didScroll;
+  let lastScrollTop = 0;
+  let delta = 5;
+  let navbarHeight = $('header').outerHeight();
 
-    function showSideNav() {
-        isOpen = true;
-        document.getElementById("nav-ul").style.transform = 'translateX(0px)';
-        document.getElementsByClassName("overlay")[0].classList.add("opened");
-        document.getElementById("body").style.overflowY = 'hidden';
+  function showSideNav() {
+    isOpen = true;
+    document.getElementById("nav-ul").style.transform = 'translateX(0px)';
+    document.getElementsByClassName("overlay")[0].classList.add("opened");
+    document.getElementById("body").style.overflowY = 'hidden';
+  }
+
+  function hideSideNav() {
+    if (isOpen) {
+      document.getElementById("nav-ul").style.transform = 'translateX(-365px)';
+      isOpen = false;
+      document.getElementsByClassName("overlay")[0].classList.remove("opened");
+      document.getElementById("body").style.overflowY = 'unset';
     }
+  }
 
-    function hideSideNav() {
-        if(isOpen) {
-            document.getElementById("nav-ul").style.transform = 'translateX(-365px)';
-            isOpen = false;
-            document.getElementsByClassName("overlay")[0].classList.remove("opened");
-            document.getElementById("body").style.overflowY = 'auto';
-          }
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    if (Math.abs(lastScrollTop - st) <= delta)
+      return;
+
+    if (st > lastScrollTop && st > navbarHeight) {
+      // Scroll Down
+      $('header').removeClass('nav-down').addClass('nav-up');
+      $('#menu').removeClass('nav-down').addClass('nav-up');
+    } else {
+      // Scroll Up
+      if (st + $(window).height() < $(document).height()) {
+        $('header').removeClass('nav-up').addClass('nav-down');
+        $('#menu').removeClass('nav-up').addClass('nav-down');
+      }
     }
+    lastScrollTop = st;
+  }
 
-    function initialize() {
-        var event = setEventListners();
-        event.setClickEventById("menu", showSideNav);
-        event.setClickEventById("body", hideSideNav)
-        setIntersectionObserver();
-    }
+  function initialize() {
+    var event = setEventListners();
+    event.setClickEventById("menu", showSideNav);
+    event.setClickEventById("body", hideSideNav)
+    //setIntersectionObserver();
+    $(window).scroll(function () {
+      didScroll = true;
+    });
+    setInterval(function () {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 250);
+  }
 
-    initialize();
+  initialize();
 
-    function setIntersectionObserver() {
-        document.addEventListener("DOMContentLoaded", function() {
-            var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-          
-            if ("IntersectionObserver" in window) {
-              let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-                entries.forEach(function(entry) {
-                  if (entry.isIntersecting) {
-                    let lazyImage = entry.target;
-                    lazyImage.src = lazyImage.dataset.src;
-                    lazyImageObserver.unobserve(lazyImage);
-                    lazyImage.onload = function() {
-                        lazyImage.classList.remove("lazy");
-                    }
-                  }
-                });
-              });
-          
-              lazyImages.forEach(function(lazyImage) {
-                lazyImageObserver.observe(lazyImage);
-              });
-            } else {
-              // Possibly fall back to a more compatible method here
+  function setIntersectionObserver() {
+    document.addEventListener("DOMContentLoaded", function () {
+      var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+      if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+          entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+              let lazyImage = entry.target;
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImageObserver.unobserve(lazyImage);
+              lazyImage.onload = function () {
+                lazyImage.classList.remove("lazy");
+              }
             }
           });
-    }
+        });
 
-    function setEventListners() {
-        return {
-            setClickEventById: function(id, callback) {
-                document.getElementById(id).addEventListener("click", callback);
-            }
-        }
+        lazyImages.forEach(function (lazyImage) {
+          lazyImageObserver.observe(lazyImage);
+        });
+      } else {
+        // Possibly fall back to a more compatible method here
+      }
+    });
+  }
+
+  function setEventListners() {
+    return {
+      setClickEventById: function (id, callback) {
+        document.getElementById(id).addEventListener("click", callback);
+      }
     }
+  }
 })()
